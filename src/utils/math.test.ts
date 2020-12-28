8067a480654f8806267529999a068489894d58a9
Lucas <luukasa2@hotmail.com>
2020-12-28 20:33:35 +0200

    improved math tests based on real calculations, fixed distance calculation and added better comments/resources

--- src/utils/math.test.ts
+++ src/utils/math.test.ts
@@ -1,12 +1,17 @@
 import { getDistanceBetweenPoints } from './math'
 
+// results checked with https://www.calculatorsoup.com/calculators/geometry-plane/distance-two-points.php
 describe('math.ts', () => {
-  test('getDistanceBetweenPoints(), zero', async () => {
+  test('getDistanceBetweenPoints()', async () => {
     const distance = getDistanceBetweenPoints(0, 0, 0, 0)
     expect(distance).toStrictEqual(0)
   })
-  test('getDistanceBetweenPoints(), ', async () => {
+  test('getDistanceBetweenPoints(0, 0, 1, 1)', async () => {
     const distance = getDistanceBetweenPoints(0, 0, 1, 1)
-    expect(distance).toStrictEqual(1)
+    expect(distance).toStrictEqual(1.41)
+  })
+  test('getDistanceBetweenPoints(0, 0, 4, 4)', async () => {
+    const distance = getDistanceBetweenPoints(0, 0, 4, 4)
+    expect(distance).toStrictEqual(5.66)
   })
 })
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,12 +1,16 @@
-/** Uses the two dimensional distance formula https://brilliant.org/wiki/distance-formula */
+/** Uses the two dimensional distance formula https://en.wikipedia.org/wiki/Euclidean_distance,
+ * returning result in two decimal precision */
 const getDistanceBetweenPoints = (
   x0: number,
   y0: number,
   x1: number,
   y1: number
 ): number => {
-  const distance = Math.sqrt((Math.pow(x0 - x1, 2), Math.pow(y0 - y1, 2)))
-  return distance
+  const distance = Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2))
+  // method https://stackoverflow.com/a/11832950
+  const distanceTwoDecimals =
+    Math.round((distance + Number.EPSILON) * 100) / 100
+  return distanceTwoDecimals
 }
 
 export { getDistanceBetweenPoints }
